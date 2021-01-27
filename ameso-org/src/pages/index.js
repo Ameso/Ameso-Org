719cb2803b3b2fd60253cff91c6eea0fb5396ba2
simon <sim.liu92@gmail.com>
2021-01-26 23:39:27 -0500

    Initial gatsby commit

---
+++ ameso-org/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const IndexPage = props => {
+  // to do : implement dark mode
+
+  return (
+    <div>
+      Ameso is a decentralized protocol for employers to hire contractors and guarantee quality of work.
+    </div>
+  )
+}
+
+export default IndexPage
